8efc222deb2ddabb0e55eb8bc39bc9d284f63083
Vincent zhong <vzhong@vmware.com>
2022-09-23 14:06:39 +0800

    init db

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+// Learn GO
+// Go reference and value
+
+
+func main() {
+  var i int
+
+  fmt.Print("Type a number: ")
+  fmt.Scan(&i)
+  fmt.Println("Your number is:", i)
+}
